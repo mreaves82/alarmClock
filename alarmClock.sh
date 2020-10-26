cb356bb081119e72b6bcf9eb3ac48f24895be479
famousAmous <famousamous@localhost.localdomain>
2020-10-26 13:27:58 -0400

    alarmClock.sh

--- alarmClock.sh
+++ alarmClock.sh
@@ -5,10 +5,7 @@ echo by Matt Reaves
 #varialbes for increments
 i=0
 t=0
-#array for random song selection 
-randomSongs=("abbaDancingQueen.mp3" "beatlesSgtPepper.mp3" 
-	"petshopboysOpportunites.mp3" "thehumanleagueDontYouWantMe.mp3" 
-	"blondieHeartOfGlass.mp3")
+
 #ask for user name
 echo What is the users name?
    read -t 5 name1
@@ -47,31 +44,41 @@ echo Hi $name1.... How long do you wish to set the alarm for?
    	   
    	   done
 
- PS3=$name1", please select an alarm option."
- choices=("The Beatles - Good Morning" "The Clash - Rock The Casbah"
- 	"David Bowie - Ashes To Ashes" "Pet Shop Boys - West End Girls" 
- 	"Surprise Me")
- path1='~/Scripts/alarmClock/audio/'
- select choice in "This Choice" ${choices[@]}
+#array for random song selection 
+randomSongs=("abbaDancingQueen.mp3" "beatlesSgtPepper.mp3" 
+	"petshopboysOpportunites.mp3" "thehumanleagueDontYouWantMe.mp3" 
+	"blondieHeartOfGlass.mp3")
+
+PS3=$name1", please select an alarm option."
+choice1='The Beatles - Good Morning';
+choice2="The Clash - Rock The Casbah";
+choice3="David Bowie - Ashes To Ashes";
+choice4="Pet Shop Boys - West End Girls";
+choice5="Surprise Me";
+choices=($choice1 $choice2 $choice3 $choice4 $choice5)
+path1="~/Scripts/alarmClock/audio/"
+select choice in 'The Beatles - Good Morning' "The Clash - Rock The Casbah" 
+    "David Bowie - Ashes To Ashes" "Pet Shop Boys - West End Girls" "Surprise Me"
  	do
  	   case $choice in
- 	     "The Beatles - Good Morning")
-			sleep $time1 && vlc $path1"beatlesGoodMorning.mp3"
+ 	      $choice1)
+		    sleep $time1 && vlc ~/Scripts/alarmClock/audio/beatlesGoodMorning.mp3
 			break
 			;;
-		 "The Clash - Rock The Casbah") 
-			sleep $time1 && vlc $path1
+		 $choice2) 
+			sleep $time1 && vlc ~/Scripts/alarmClock/audio/theclashRockTheCasbah.mp3
 			break
 			;;
-	     "David Bowie - Ashes To Ashes")
-			sleep $time1 && vlc $path1
+	     $choice3)
+			sleep $time1 && vlc ~/Scripts/alarmClock/audio/davidbowieAshesToAshes.mp3
 			break
 			;;
-	     "Pet Shop Boys - West End Girls")
-			sleep $time1 && vlc $path1
+	     $choice4) 
+			sleep $time1 && vlc ~/Scripts/alarmClock/audio/petshopboysWestEndGirls.mp3
 			break
 			;;
-	     "Surprise Me")
+	     $choice5)
+			sleep $time1 && vlc ~/Scripts/alarmClock/audio/${randomSongs[$(( $RANDOM % 5 +1 ))]}
 		esac
 
 	done
